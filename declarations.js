816cd05ed38a3647bee3bc16989cffac1250369e
catom <tom.calvo@ynov.com>
2024-02-28 15:43:42 +0100

    Declaration V.2

--- declarations.js
+++ declarations.js
@@ -2,3 +2,4 @@ const escapeStr = "`, \\, /, \" '";
 const arr = [4, '2'];
 const obj = {str: "string", num: 27, bool: true, undef: undefined};
 const nested= {arr: [4, undefined, '2'], obj: {str: "string", num: 27, bool: true}};
+Object.freeze(obj,arr,nested);
