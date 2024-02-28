d1bbdb712f1bd72ea1a986e36fa0e8f03ddf8146
catom <tom.calvo@ynov.com>
2024-02-28 15:46:11 +0100

    Declaration V.4

--- declarations.js
+++ declarations.js
@@ -4,4 +4,5 @@ const obj = {str: "string", num: 27, bool: true, undef: undefined};
 const nested= {arr: [4, undefined, '2'], obj: {str: "string", num: 27, bool: true}};
 Object.freeze(obj);
 Object.freeze(arr);
-Object.freeze(nested);
+Object.freeze(nested.arr);
+Object.freeze(nested.obj);
