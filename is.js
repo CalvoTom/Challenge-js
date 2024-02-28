b4f11965769a3cb054734e7a2b1f96b4c3b0c24a
catom <tom.calvo@ynov.com>
2024-02-28 17:53:26 +0100

    Is V.1

---
+++ is.js
@@ -0,0 +1,35 @@
+const is = {
+    num: function(value) {
+        return typeof value === 'number';
+    },
+    nan: function(value) {
+        return isNaN(value);
+    },
+    str: function(value) {
+        return typeof value === 'string';
+    },
+    bool: function(value) {
+        return typeof value === 'boolean';
+    },
+    undef: function(value) {
+        return typeof value === 'undefined';
+    },
+    def: function(value) {
+        return typeof value !== 'undefined';
+    },
+    arr: function(value) {
+        return Array.isArray(value);
+    },
+    obj: function(value) {
+        return value !== null && typeof value === 'object';
+    },
+    fun: function(value) {
+        return typeof value === 'function';
+    },
+    truthy: function(value) {
+        return Boolean(value);
+    },
+    falsy: function(value) {
+        return !Boolean(value);
+    }
+};
