9559d5c30704f11faa4a1eba77ce2aea082f9f54
catom <tom.calvo@ynov.com>
2024-03-11 10:01:29 +0100

    IndexOf V.4

--- index-of.js
+++ index-of.js
@@ -15,10 +15,14 @@ function lastIndexOf(array, value, index = array.length-1){
     return -1;
 }
 function includes(array, value){
-    for (let i of array){
+    for (let i = 0; i < array.length; i++){
         if (array[i] === value){
             return true;
         }
     }
     return false;
 }
+
+console.log(indexOf([0,3,1,4,3],3,2));
+console.log(lastIndexOf(['t',1,4,'t'],'t', 2));
+console.log(includes([0, 0, 't', 't'], 't'));
