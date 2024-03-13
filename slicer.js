83f0fe28e5a6e341da5644bf2a9c34db1420b202
catom <tom.calvo@ynov.com>
2024-03-13 11:30:18 +0100

    Slicer V.2

--- slicer.js
+++ slicer.js
@@ -1,12 +1,27 @@
 function slice (str, startIndex, EndIndex = str.length){
-    let result = [];
-    if((!EndIndex) || (EndIndex > str.length)) {
-        for(let i = startIndex; i<str.length; i++) {
-            result.push(str[i]);}
-    } else {
-        for(let i = startIndex; i<EndIndex; i++) {
-            result.push(str[i]);
+    if (typeof str === "string"){
+        let result = "";
+        if((!EndIndex) || (EndIndex > str.length)) {
+            for(let i = startIndex; i<str.length; i++) {
+                result = result + str[i];
+            }
+        } else {
+            for(let i = startIndex; i<EndIndex; i++) {
+                result = result + str[i];
+            }
         }
+        return result;
+    }else {
+        let result = [];
+        if ((!EndIndex) || (EndIndex > str.length)) {
+            for (let i = startIndex; i < str.length; i++) {
+                result.push(str[i]);
+            }
+        } else {
+            for (let i = startIndex; i < EndIndex; i++) {
+                result.push(str[i]);
+            }
+        }
+        return result;
     }
-    return result;
 }
