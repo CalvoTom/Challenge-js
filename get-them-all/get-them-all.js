590d93eac62aab162ce7aa23a575b0ba9ed094aa
catom <tom.calvo@ynov.com>
2024-03-15 10:36:36 +0100

    Get them all V.1

--- get-them-all/get-them-all.js
+++ get-them-all/get-them-all.js
@@ -1,15 +1,27 @@
 export function getArchitects (){
-
+    const ArrayTagA = document.getElementsByTagName("a")
+    const ArrayNonArchitect = document.getElementsByTagName("span")
+    const Array = [ArrayTagA, ArrayNonArchitect]
+    return Array
 }
 
 export function getClassical(){
-
+    const ArrayClassClassical = document.getElementsByClassName("classical")
+    const ArrayNonClassical = document.getElementsByClassName("modern baroque")
+    const Array = [ArrayClassClassical, ArrayNonClassical]
+    return Array
 }
 
 export function getActive (){
-
+    const ArrayClassClassicalActive = document.querySelectorAll("a.classical.active")
+    const ArrayClassClassicalNotActive = document.querySelectorAll("a.classical:not(.active)")
+    const Array = [ArrayClassClassicalActive, ArrayClassClassicalNotActive]
+    return Array
 }
 
 export function getBonannoPisano (){
-
+    const IdBonannoPisano = document.getElementById("BonannoPisano")
+    const ArrayClassClassicalActive = document.querySelectorAll("a.classical.active")
+    const Array = [IdBonannoPisano, ArrayClassClassicalActive]
+    return Array
 }
